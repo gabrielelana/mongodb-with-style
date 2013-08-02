f4abd33c9a5d6e205a8af4cdffa6a62e9294c766
gabriele.lana <gabriele.lana@cleancode.it>
2013-08-02 08:44:36 +0200

    fixed typo memory <-> index

--- queries/scan_and_order.js
+++ queries/scan_and_order.js
@@ -1,10 +1,10 @@
 // we want all the transactions from an user ordered by creation time, and we want to use an index
 db.transactions.drop()
 db.transactions.insert({user: 'gabriele', amount: 1, created_at: ISODate()})
-db.transactions.insert({user: 'gabriele', amount: 1, created_at: ISODate()})
-db.transactions.insert({user: 'gabriele', amount: 1, created_at: ISODate()})
-db.transactions.insert({user: 'francesco', amount: 1, created_at: ISODate()})
-db.transactions.insert({user: 'francesco', amount: 1, created_at: ISODate()})
+db.transactions.insert({user: 'gabriele', amount: 2, created_at: ISODate()})
+db.transactions.insert({user: 'gabriele', amount: 4, created_at: ISODate()})
+db.transactions.insert({user: 'francesco', amount: 2, created_at: ISODate()})
+db.transactions.insert({user: 'francesco', amount: 6, created_at: ISODate()})
 db.transactions.insert({user: 'giorgio', amount: 1, created_at: ISODate()})
 
 // try with two indexes
@@ -26,7 +26,7 @@ result = db.transactions.find({user: 'gabriele'}).sort({created_at: 1}).explain(
 
 print('With one compound index')
 assert(/^BtreeCursor/.test(result.cursor), 'it uses an index to get back results')
-assert(!result.scanAndOrder, 'sort is done in memory yay!')
+assert(!result.scanAndOrder, 'sort is done with index yay!')
 
 
 // the 'query sort order' and the 'index sort order' do not matter
@@ -34,7 +34,7 @@ result = db.transactions.find({user: 'gabriele'}).sort({created_at: -1}).explain
 
 print('With one compound index in reverse order')
 assert(/reverse$/.test(result.cursor), 'it uses the index in reverse order')
-assert(!result.scanAndOrder, 'sort is done in memory yay!')
+assert(!result.scanAndOrder, 'sort is done with index yay!')
 
 // if you only need to count then you can select only fields in index
 db.transactions.dropIndexes()
@@ -43,5 +43,5 @@ result = db.transactions.find({user: 'gabriele'}, {user: 1, _id: 0}).sort({creat
 
 print('Only compund index to do everything')
 assert.eq(0, result.nscannedObjects, 'no need to load found objects in memory')
-assert(!result.scanAndOrder, 'sort is done in memory yay!')
+assert(!result.scanAndOrder, 'sort is done with index yay!')
 assert(result.indexOnly, 'all done with the index!')
